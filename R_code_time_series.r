0b24fe443a895282bf797506b83c6f2107c17659
stora86 <70709431+stora86@users.noreply.github.com>
2021-04-02 11:41:29 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,9 @@
+# Time series analys
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+#install the packages and call the packages
+# install.packages("raster")
+library(raster)
+#set the directory
+setwd("C:/lab/greenland")
